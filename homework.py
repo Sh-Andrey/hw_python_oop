e448de9d2f591ebfd91a57f9ac438223e6614c69
Sh-Andrey <sh.a-n-d-r-e-y@yandex.ru>
2021-03-17 13:46:55 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,39 +1,37 @@
 import datetime as dt
+DATE_FORMAT: str = "%d.%m.%Y"
 
 
 class Record:
-    """Создаем записи.
+    """Создаем записи для класса Calculator.
 
     Атрибуты класса:
     - число amount (денежная сумма или количество килокалорий);
     - комментарий comment, поясняющий, на что потрачены деньги
     или откуда взялись калории;
-    - создания записи date (передаётся в явном виде в конструктор,
+    - дата создания записи (передаётся в явном виде в конструктор,
     либо присваивается значение по умолчанию — текущая дата).
     """
-    def __init__(self, amount: float, comment: str, date=None) -> None:
-
+    def __init__(self, amount: float, comment: str, date: str = None) -> None:
         self.amount = amount
         self.comment = comment
-        date_format: str = "%d.%m.%Y"
         if date is None:
             self.date = dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, date_format).date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
 
 
 class Calculator:
+    """Общая функциональность для классов
+    CashCalculator и CaloriesCalculator.
 
+    Атрибуты класса:
+    - число limit (дневной лимит трат/калорий, который задал пользователь);
+    - для хранения записей, создаем пустой список records.
+    """
     def __init__(self, limit: float) -> None:
-        """Общая функциональностью для классов
-        CashCalculator и CaloriesCalculator.
-
-        Атрибуты класса:
-        - число limit (дневной лимит трат/калорий, который задал пользователь);
-        - для хранение записей, создаем пустой список records.
-        """
         self.limit = limit
-        self.records: list = []
+        self.records: list[float, str, str] = []
 
     def add_record(self, record: float) -> None:
         """
@@ -45,22 +43,22 @@ class Calculator:
         """
         Метод считает, сколько денег/калорий уже съедено сегодня.
         """
-        date_now = dt.date.today()
-        count = sum([record.amount for record in self.records
-                     if record.date == date_now])
+        today = dt.date.today()
+        count = sum(record.amount for record in self.records
+                    if record.date == today)
         return count
 
     def get_week_stats(self) -> float:
         """
         Метод считает, сколько денег/калорий потрачено за последние 7 дней.
         """
-        date_now = dt.date.today()
-        week = date_now - dt.timedelta(days=6)
-        count = sum([record.amount for record in self.records
-                     if date_now >= record.date >= week])
+        today = dt.date.today()
+        recent_days = today - dt.timedelta(days=7)
+        count = sum(record.amount for record in self.records
+                    if recent_days < record.date <= today)
         return count
 
-    def limit_today(self) -> float:
+    def remainder_today(self) -> float:
         """
         Метод считает, сколько денег/калорий осталось на день.
         """
@@ -73,7 +71,7 @@ class CaloriesCalculator(Calculator):
         """
         Метод определяет, сколько ещё калорий можно/нужно получить сегодня.
         """
-        calories_today: float = self.limit_today()
+        calories_today: float = self.remainder_today()
         if calories_today > 0:
             return ("Сегодня можно съесть что-нибудь ещё,"
                     " но с общей калорийностью не более "
@@ -82,7 +80,6 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
-
     USD_RATE: float = 73.45
     EURO_RATE: float = 87.34
     RUB_RATE: float = 1.00
@@ -91,27 +88,27 @@ class CashCalculator(Calculator):
         """
         Метод возвращает сообщения о состоянии дневного баланса.
         """
-        cash_sum: float = self.limit_today()
+        cash_sum: float = self.remainder_today()
         cash_currency: str = currency.lower()
-        cash_type: list = ["usd", "eur", "rub"]
-
-        if cash_currency not in cash_type:
-            return f"{currency} такой валюты нету!"
+        cash_type: tuple = ("usd", "eur", "rub")
 
         if cash_sum == 0:
             return "Денег нет, держись"
 
-        if cash_currency in cash_type:
-            dict_currency = {
-                "usd": (cash_sum / self.USD_RATE, "USD"),
-                "eur": (cash_sum / self.EURO_RATE, "Euro"),
-                "rub": (cash_sum / self.RUB_RATE, "руб"),
-            }
-            cash_dict: float = dict_currency[currency][0]
-            type_dict: str = dict_currency[currency][1]
-            cash_dict: float = abs(round(cash_dict, 2))
+        if cash_currency not in cash_type:
+            return f"{currency} такой валюты нету!"
 
-            if cash_sum < 0:
-                return ("Денег нет, держись: "
-                        f"твой долг - {cash_dict} {type_dict}")
-            return f"На сегодня осталось {cash_dict} {type_dict}"
+        currency_type: dict = {
+            "usd": (self.USD_RATE, "USD"),
+            "eur": (self.EURO_RATE, "Euro"),
+            "rub": (self.RUB_RATE, "руб"),
+        }
+
+        after_exchange, cash_type = currency_type[currency]
+        cash_sum: float = cash_sum / after_exchange
+        cash_sum = round(cash_sum, 2)
+
+        if cash_sum < 0:
+            return ("Денег нет, держись: "
+                    f"твой долг - {abs(cash_sum)} {cash_type}")
+        return f"На сегодня осталось {cash_sum} {cash_type}"
